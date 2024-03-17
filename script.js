50f9f0e1be0178eb770bd8d6ee0e7a5d5da45a69
motty <mlc40622@gmail.com>
2024-03-17 13:48:41 +0200

    how its works -> unresponsible

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const globalVariables = {
     hamburgerClicked: true,
     headerSubNavDropboxClicked: false,
+    howItsWorkStep01Clicked: false,
+    howItsWorkStep02Clicked: false,
+    howItsWorkStep03Clicked: false,
+    howItsWorkStep04Clicked: false,
 }
 document.getElementById("desktop-header-sub-nav-link").addEventListener('mouseover', function() {
     document.getElementById("desktop-header-sub-nav").classList.remove("display-none");
@@ -13,6 +17,80 @@ document.getElementById("desktop-header-sub-nav-link").addEventListener('mousele
 
         document.getElementById("desktop-header-sub-nav").classList.add("display-none");
 });
+
+
+function step01button(){
+    if(!globalVariables.howItsWorkStep01Clicked){     
+        document.getElementById("step01-content").classList.remove("display-none");
+        setTimeout(() => {
+            document.getElementById("step01-content").classList.remove("max-height-0");
+        }, 20);
+        globalVariables.howItsWorkStep01Clicked = true;
+        document.getElementById("step-header-button-01").classList.add("rotate-right-45deg");
+    }else{
+        document.getElementById("step01-content").classList.add("max-height-0");
+        setTimeout(() => {
+            document.getElementById("step01-content").classList.add("display-none");
+        }, 300); 
+        globalVariables.howItsWorkStep01Clicked = false;
+        document.getElementById("step-header-button-01").classList.remove("rotate-right-45deg");
+    }
+}
+function step02button(){
+    if(!globalVariables.howItsWorkStep02Clicked){     
+        document.getElementById("step02-content").classList.remove("display-none");
+        setTimeout(() => {
+            document.getElementById("step02-content").classList.remove("max-height-0");
+        }, 20);
+        globalVariables.howItsWorkStep02Clicked = true;
+        document.getElementById("step-header-button-02").classList.add("rotate-right-45deg");
+    }else{
+        document.getElementById("step02-content").classList.add("max-height-0");
+        setTimeout(() => {
+            document.getElementById("step02-content").classList.add("display-none");
+        }, 300); 
+        globalVariables.howItsWorkStep02Clicked = false;
+        document.getElementById("step-header-button-02").classList.remove("rotate-right-45deg");
+    }
+}
+function step03button(){
+    if(!globalVariables.howItsWorkStep03Clicked){     
+        document.getElementById("step03-content").classList.remove("display-none");
+        setTimeout(() => {
+            document.getElementById("step03-content").classList.remove("max-height-0");
+        }, 20);
+        globalVariables.howItsWorkStep03Clicked = true;
+        document.getElementById("step-header-button-03").classList.add("rotate-right-45deg");
+    }else{
+        document.getElementById("step03-content").classList.add("max-height-0");
+        setTimeout(() => {
+            document.getElementById("step03-content").classList.add("display-none");
+        }, 300); 
+        globalVariables.howItsWorkStep03Clicked = false;
+        document.getElementById("step-header-button-03").classList.remove("rotate-right-45deg");
+    }
+}
+function step04button(){
+    if(!globalVariables.howItsWorkStep04Clicked){     
+        document.getElementById("step04-content").classList.remove("display-none");
+        setTimeout(() => {
+            document.getElementById("step04-content").classList.remove("max-height-0");
+        }, 20);
+        globalVariables.howItsWorkStep04Clicked = true;
+        document.getElementById("step-header-button-04").classList.add("rotate-right-45deg");
+    }else{
+        document.getElementById("step04-content").classList.add("max-height-0");
+        setTimeout(() => {
+            document.getElementById("step04-content").classList.add("display-none");
+        }, 300); 
+        globalVariables.howItsWorkStep04Clicked = false;
+        document.getElementById("step-header-button-04").classList.remove("rotate-right-45deg");
+    }
+}
+
+
+
+
 
 function headerSubNavDropbox(){
     if(!globalVariables.headerSubNavDropboxClicked){
